d29825c5be0413d1fe41cce182d830a8b4166777
Kent WU <twer@Kent-Wu-2.local>
2014-11-16 23:31:45 +0800

    extract method from main

--- robot/Client.java
+++ robot/Client.java
@@ -1,45 +1,52 @@
 package robot;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
 public class Client {
 	public static void main(String[] args) throws Exception {
-		Robot r=new Robot();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-        String strBoundary = null; 
-        System.out.println("Enter your Boundary x and y:"); 
-        strBoundary = br.readLine();
-        String[] boundaryArr=strBoundary.split(" ");
-        if(!idDigit(boundaryArr[0]) || !idDigit(boundaryArr[1])){
-        	System.out.println("Boundary x and y are must be digit,and input them again:"); 
-        }
-        Robot.X=Integer.parseInt(boundaryArr[0]);
-        Robot.Y=Integer.parseInt(boundaryArr[1]);
-        String strInitPos = null; 
-        System.out.println("Enter your robot initial x,y and direstion:"); 
-        strInitPos = br.readLine();
-        String[] initPos=strInitPos.split(" ");
-
-        r.x=Integer.parseInt(initPos[0]);
-        r.y=Integer.parseInt(initPos[1]);
-        r.dir=Character.toUpperCase(initPos[2].charAt(0));
-        String strOrder = null;
+		Robot robot=new Robot();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        init(br);
+        initRobot(robot, br);
         while(true){
-        	
-            System.out.println("Enter your Robot orders:"); 
-            strOrder = br.readLine();
-            String result=r.control(strOrder);
-            System.out.println(result);
-            if("RIP".equals(result)){
-            	System.exit(0);
-            }
+            outputResult(robot, br);
         }
         
 	}
 
-	public static boolean idDigit(String str){
+    private static void outputResult(Robot robot, BufferedReader br) throws IOException {
+        System.out.println("Enter your Robot orders:");
+
+        String result=robot.control(br.readLine());
+
+        System.out.println(result);
+    }
+
+    private static void initRobot(Robot robot, BufferedReader br) throws IOException {
+        System.out.println("Enter your robot initial x,y and direstion:");
+        String[] initPos= br.readLine().split(" ");
+
+        robot.x=Integer.parseInt(initPos[0]);
+        robot.y=Integer.parseInt(initPos[1]);
+        robot.dir=Character.toUpperCase(initPos[2].charAt(0));
+    }
+
+    private static void init(BufferedReader br) throws IOException {
+        System.out.println("Enter your Boundary x and y:");
+        String[] boundaryArr= br.readLine().split(" ");
+
+        if(!idDigit(boundaryArr[0]) || !idDigit(boundaryArr[1])){
+        	System.out.println("Boundary x and y are must be digit,and input them again:");
+        }
+        
+        Robot.X=Integer.parseInt(boundaryArr[0]);
+        Robot.Y=Integer.parseInt(boundaryArr[1]);
+    }
+
+    public static boolean idDigit(String str){
 		Pattern pattern = Pattern.compile("[0-9]*");
 	    return pattern.matcher(str).matches();
 	}
